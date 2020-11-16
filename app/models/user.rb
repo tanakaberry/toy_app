0eac70f37342752d943b415f17c3002b5c358edb
Ryuichi Tanaka <tanaka-ryuichi@tanaka-ryuichi.freee.local>
2020-11-17 02:30:39 +0900

    finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts
+    validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
+    validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
